df0801fd500292a7e60365587f7bf4ea90edc7ba
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-19 16:40:31 -0500

    Read meters as double-precision float

--- Program.cs
+++ Program.cs
@@ -7,14 +7,26 @@ namespace metric_to_imperial
     {
         // use class-level static variables, since
         // we are not instantiating objects (yet!)
-        static int meters,
-                   totalInches,
+        static double meters;
+        static int totalInches,
                    feet,
                    inches;
         static void Main(string[] args)
         {
+            ReadInt("How many meters?");
             WriteLine("Hello World!");
         }
-
+        static double ReadDouble(string label)
+        {
+            Write("{0} ", label);
+            double number;
+            string userInput = ReadLine();
+            while (Double.TryParse(userInput, out number) == false)
+            {
+                Write("{0} {1} ", "Invalid input!", label);
+                userInput = ReadLine();
+            }
+            return number;
+        }
     }
 }
