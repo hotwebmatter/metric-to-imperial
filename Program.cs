209552baa77e9462c5ae1466ce3ba96d3cf70c31
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-19 16:35:38 -0500

    Declare class-level static variables

--- Program.cs
+++ Program.cs
@@ -5,9 +5,16 @@ namespace metric_to_imperial
 {
     class Program
     {
+        // use class-level static variables, since
+        // we are not instantiating objects (yet!)
+        static int meters,
+                   totalInches,
+                   feet,
+                   inches;
         static void Main(string[] args)
         {
             WriteLine("Hello World!");
         }
+
     }
 }
