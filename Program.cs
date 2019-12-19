5493855994ce07f726eb60c399585ac2f501386b
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-19 16:26:13 -0500

    Initial commit of metric-to-imperial project scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace metric_to_imperal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
